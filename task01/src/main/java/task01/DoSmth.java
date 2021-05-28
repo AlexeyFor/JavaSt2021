20b0caaf523778487358faccea0b7a6eccd71eca
Alexey <aleksyforepam@gmail.com>
2021-05-28 21:44:24 +0300

    add task01/

---
+++ task01/src/main/java/task01/DoSmth.java
@@ -0,0 +1,7 @@
+package task01;
+
+public class DoSmth {
+public static void main (String args []) {
+	System.out.println( "hello moto");
+}
+}
---
+++ task02/src/main/java/task02/DoMain.java
@@ -0,0 +1,7 @@
+package task02;
+
+public class DoMain {
+	public static void main (String args []) {
+		System.out.println( "hello moto, please");
+	}
+}
